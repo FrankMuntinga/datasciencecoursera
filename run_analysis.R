af29a0eba52d087567c87009f82c2552c117c37c
Frank Muntinga <frank.muntinga@ciber.com>
2015-01-25 21:09:17 +0100

    Run_analysis.R script used in the Getting and Cleaning Data course to create tidy data set

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,16 @@
-####################################################################################################
-## Script:      run_analysis.R
-## Name:        Frank Muntinga
-## Date:        23-01-2015
-## Goal:        You should create one R script called run_analysis.R that does the following. 
-##              1. Merges the training and the test sets to create one data set.
-##              2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-##              3. Uses descriptive activity names to name the activities in the data set
-##               4. Appropriately labels the data set with descriptive variable names. 
-##               5. From the data set in step 4, creates a second, independent tidy data set 
-##                 with the average of each variable for each activity and each subject.
-##
-#####################################################################################################
+#################################################################################################################
+## Script:      run_analysis.R                                                                                  #
+## Name:        Frank Muntinga                                                                                  #
+## Date:        23-01-2015                                                                                      #
+## Goal:        You should create one R script called run_analysis.R that does the following.                   #
+##              1. Merges the training and the test sets to create one data set.                                #
+##              2. Extracts only the measurements on the mean and standard deviation for each measurement.      #
+##              3. Uses descriptive activity names to name the activities in the data set                       #
+##               4. Appropriately labels the data set with descriptive variable names.                          #
+##               5. From the data set in step 4, creates a second, independent tidy data set                    #
+##                 with the average of each variable for each activity and each subject.                        #
+##                                                                                                              #
+#################################################################################################################
 ## get data from the web and download file
 dir.create("c:/work/R/downloads")
 dir.create("c:/work/R/data")
@@ -28,36 +28,36 @@ train_subj <-  read.table("./train/subject_train.txt")
 test_x <-      read.table("./test/X_test.txt")
 test_y <-      read.table("./test/Y_test.txt")
 test_subj <-   read.table("./test/subject_test.txt")
-##
+######################################################################################################
 ##        1. Merges the training and the test sets to create one data set.
-##
+######################################################################################################
 train_test_x <- rbind(train_x, test_x)
 ## changes column headers for the train_test_x data frame
+## make.names(..., unique=TRUE) needed to avoid duplicate column names during summarizing in step 5
 headers <- as.character(features[,2])
-##!!!!!!!!!!!!!!!!!!!!!!
 headers <- make.names(headers,unique=TRUE)
 names(train_test_x) <- headers
-##
+#######################################################################################################
 ##       2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-## 
+## ####################################################################################################
 col_names <- names(train_test_x)
 train_test_x_mean <- grep("mean",col_names,fixed=TRUE)
 train_test_x_stddev <- grep("std",col_names,fixed=TRUE)
 result_mean <- train_test_x[train_test_x_mean]
 result_stddev <- train_test_x[train_test_x_stddev]
 result_total <- cbind(result_mean, result_stddev)
-##
+#######################################################################################################
 ##       4. Appropriately labels the data set with descriptive variable names. 
-##
+#######################################################################################################
 col_names <- names(result_total)
 col_headers <- gsub("fBody", "Body",col_names)
 col_headers <- gsub("tBody", "Body",col_headers)
 col_headers <- gsub("tGravity", "Gravity",col_headers)
 col_headers <- gsub("\\(|\\)", "",col_headers)
 names(result_total) <- col_headers
-##
+########################################################################################################
 ##       3. Uses descriptive activity names to name the activities in the data set
-##
+########################################################################################################
 ## bind the train_y and test_y data frames
 train_test_y <- rbind(train_y, test_y)
 ## bind the subject_train and subject_test data frames
@@ -68,6 +68,7 @@ train_test_activities <- merge(train_test_y, activities,by.x= "V1",by.y="V1")
 result_total_incl_subj <- cbind(result_total,train_test_subject)
 col_names <- names(result_total_incl_subj)
 col_headers <- gsub("V1", "Subject",col_names)
+## make.names(..., unique=TRUE) needed to avoid duplicate column names during summarizing in step 5
 col_headers <- make.names(col_headers,unique=TRUE)
 names(result_total_incl_subj) <- col_headers
 ## bind the activity variable to the result_total_incl_subj dataframe and rename activity headers
@@ -75,12 +76,13 @@ result_total_incl_subj_act <- cbind(result_total_incl_subj,train_test_activities
 col_names <- names(result_total_incl_subj_act)
 col_headers <- gsub("V1", "Activity-id",col_names)
 col_headers <- gsub("V2", "Activity",col_headers)
-##!!!
+## make.names(..., unique=TRUE) needed to avoid duplicate column names during summarizing in step 5
 col_headers <- make.names(col_headers,unique=TRUE)
 names(result_total_incl_subj_act) <- col_headers
-##
+#########################################################################################################
 ##       5. From the data set in step 4, creates a second, independent tidy data set 
 ##          with the average of each variable for each activity and each subject.
+#########################################################################################################
 install.packages("dplyr")
 library(dplyr)
 result_dplyr <- tbl_df(result_total_incl_subj_act)
